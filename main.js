f642dd3a2836cd33db109ec7f803131e46742d2d
Mahboobeh Ghasemi <mah9372gh@gmail.com>
2023-07-08 15:36:05 +0330

    third practic

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ var app = new Vue ({
     description: 'A pair of warm, fuzzy socks',
     aTag: 'https://www.w3schools.com/',
     imageSource: 'https://img.freepik.com/free-photo/beautiful-single-isolated-pink-rose-white_181624-25682.jpg?w=826&t=st=1688814982~exp=1688815582~hmac=69dd3bebae02ccf61025cf23cadd15b85322cb7c113f14528cfcc0890fa77d46',
-    imageAlt: 'pink-flower'
+    imageAlt: 'pink-flower',
+    inStock: true,
+    inventory: 5,
+    show: false,
+    onSale: true,
   }
 })
