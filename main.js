4735280a7bcbb415efef6d425bf8cb02f7d0a614
Mahboobeh Ghasemi <mah9372gh@gmail.com>
2023-07-09 14:50:29 +0330

    adde assets folder

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var app = new Vue ({
   el: '#app',
   data: {
-    product: 'flower',
+    product: 'Socks',
     description: 'A pair of warm, fuzzy socks',
     aTag: 'https://www.w3schools.com/',
-    imageSource: 'https://img.freepik.com/free-photo/beautiful-single-isolated-pink-rose-white_181624-25682.jpg?w=826&t=st=1688814982~exp=1688815582~hmac=69dd3bebae02ccf61025cf23cadd15b85322cb7c113f14528cfcc0890fa77d46',
-    imageAlt: 'pink-flower',
+    imageSource: 'asset/vmSocks-green.png',
+    imageAlt: 'green-socks',
     inStock: true,
     inventory: 5,
     show: false,
