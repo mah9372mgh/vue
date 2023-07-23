e945665afc5e399d06a189812af1c7cf0779ce65
Mahboobeh Ghasemi <mah9372gh@gmail.com>
2023-07-23 18:15:55 +0330

    list rendering practice

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-var app = new Vue ({
-  el: '#app',
+var app = new Vue({
+  el: "#app",
   data: {
-    product: 'Socks',
-    description: 'A pair of warm, fuzzy socks',
-    aTag: 'https://www.w3schools.com/',
-    imageSource: 'asset/vmSocks-green.png',
-    imageAlt: 'green-socks',
+    product: "Socks",
+    description: "A pair of warm, fuzzy socks",
+    aTag: "https://www.w3schools.com/",
+    imageSource: "asset/vmSocks-green.png",
+    imageAlt: "green-socks",
     inStock: true,
     inventory: 5,
     show: false,
     onSale: true,
-  }
-})
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2233,
+        variantColor: "green",
+      },
+      {
+        variantId: 2234,
+        variantColor: "blue",
+      },
+    ],
+    sizes: ["38", "40", "42"],
+  },
+});
